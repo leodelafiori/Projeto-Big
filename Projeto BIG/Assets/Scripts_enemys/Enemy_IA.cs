d27ee6798c30a130aa392f80ee82efbfe745da73
leopaivaleo <38993618+leopaivaleo@users.noreply.github.com>
2018-09-04 14:04:49 -0300

    teste

---
+++ Projeto BIG/Assets/Scripts_enemys/Enemy_IA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_IA : MonoBehaviour {
+    [SerializeField] private float speed;
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.Translate() = new Vector3(target.transform.position.x, target.transform.position, );
+	}
+}
